99115d02a2e9cf9061efd3c8c137ffc69ba09e30
Jose Vargas <josevargasserrano2000@gmail.com>
2022-06-16 00:43:53 -0400

    ....

--- db/servicio_tecnico.sql
+++ db/servicio_tecnico.sql
@@ -18,9 +18,9 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `servicio_system`
+-- Base de datos: `servicio_tecnico`
 --
-CREATE DATABASE `servicio_tecnico`;
+
 
 -- --------------------------------------------------------
 
